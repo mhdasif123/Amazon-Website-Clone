7835e17d4eb8f7bc43b637c9d0e96ad372c4c6c9
Mohammed Asif <97513284+mhdasif123@users.noreply.github.com>
2024-05-10 17:02:05 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const imgs = document.querySelectorAll('.header-slider ul img');
+const prev_btn = document.querySelector('.control_prev');
+const next_btn = document.querySelector('.control_next');
+
+let n = 0;
+
+function changeSlide(){
+    for (let i = 0; i < imgs.length; i++) {
+        imgs[i].style.display = 'none'; 
+    }
+    imgs[n].style.display = 'block';
+}
+changeSlide();
+
+next_btn,addEventListener('click', (e)=>{
+    if(n > 0){
+        n--;
+    }else{
+       n = imgs.length - 1; 
+    }
+    changeSlide();
+})
+prev_btn.addEventListener('click', (e)=>{
+    if(n < imgs.length - 1){
+        n++;
+    }else{
+       n = 0; 
+    }
+    changeSlide();
+})
+
+
+// mouse wheel Rotate
+// const scrollContainer = document.querySelectorAll('.products');
+// for (const item of scrollContainer) {
+//     item.addEventListener('wheel', (evt)=>{
+//         evt.preventDefault();
+//         DataTransferItemList.scrollLeft += evt.deltaY
+//     })
+// }
